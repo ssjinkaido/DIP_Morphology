660e6f8ee02938b23c3fa76552265433af80b6ad
ssjinkaido <53868217+ssjinkaido@users.noreply.github.com>
2020-11-29 09:07:55 +0700

    Update open_and_close.py

--- open_and_close.py
+++ open_and_close.py
@@ -1,31 +1,20 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-path = 'assets/balls.jpg'
+path = 'alphabet.png'
 img = cv2.imread(path)
-#img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-img1 = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)[1]
-img2 = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)[1]
-kernel = np.ones((3,3),np.uint8)
-erosion = cv2.erode(img1,kernel,iterations=3)
-dilate = cv2.dilate(erosion,kernel,iterations=3)
-
-dilate1 = cv2.dilate(img2,kernel,iterations=3)
-erosion1 = cv2.erode(dilate1,kernel,iterations=3)
-
-cv2.imshow("gray",img)
-cv2.imshow("binary",img1)
-cv2.imshow("erosion",erosion)
-cv2.imwrite("assets/open.jpg",dilate)
-cv2.imshow("dilate",dilate1)
-cv2.imwrite("assets/close.jpg",erosion1)
-
-cv2.imshow("erosion1",erosion1)
-cv2.imshow("dilate1",dilate1)
-cv2.imwrite("assets/close.jpg",dilate)
-
-
+# img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+img1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+img2 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+kernel = np.ones((3, 3), np.uint8)
+open=cv2.morphologyEx(img1,cv2.MORPH_OPEN,kernel)
+close=cv2.morphologyEx(img2,cv2.MORPH_CLOSE,kernel)
+cv2.imshow("gray", img)
+cv2.imshow("binary", img1)
+cv2.imwrite("assets/open.jpg", open)
+cv2.imwrite("assets/close.jpg", close)
+cv2.imshow("open", open)
+cv2.imshow("close", close)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
