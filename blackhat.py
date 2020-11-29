535fc1b61bcff1415b93485a56be1d06cda1ade0
ssjinkaido <53868217+ssjinkaido@users.noreply.github.com>
2020-11-29 08:20:14 +0700

    Update blackhat.py

--- blackhat.py
+++ blackhat.py
@@ -6,7 +6,7 @@ kernel = cv2.getStructuringElement(cv2.MORPH_RECT,
                                    filterSize)
 
 # Reading the image named 'input.jpg'
-input_image = cv2.imread("road.jpg")
+input_image = cv2.imread("assets/road.jpg")
 input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
 
 # Applying the Black-Hat operation
@@ -16,5 +16,5 @@ tophat_img = cv2.morphologyEx(input_image,
 
 cv2.imshow("original", input_image)
 cv2.imshow("tophat", tophat_img)
-cv2.imwrite("blackhat_road.jpg",tophat_img)
+cv2.imwrite("assets/blackhat_road.jpg",tophat_img)
 cv2.waitKey(5000)
