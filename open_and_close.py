08e800e86806f5005faf04fbc8a2ea3149502c51
ssjinkaido <53868217+ssjinkaido@users.noreply.github.com>
2020-11-29 08:26:08 +0700

    Update open_and_close.py

--- open_and_close.py
+++ open_and_close.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = 'balls.jpg'
+path = 'assets/balls.jpg'
 img = cv2.imread(path)
 #img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
@@ -18,13 +18,13 @@ erosion1 = cv2.erode(dilate1,kernel,iterations=3)
 cv2.imshow("gray",img)
 cv2.imshow("binary",img1)
 cv2.imshow("erosion",erosion)
-cv2.imwrite("open.jpg",dilate)
+cv2.imwrite("assets/open.jpg",dilate)
 cv2.imshow("dilate",dilate1)
-cv2.imwrite("close.jpg",erosion1)
+cv2.imwrite("assets/close.jpg",erosion1)
 
 cv2.imshow("erosion1",erosion1)
 cv2.imshow("dilate1",dilate1)
-cv2.imwrite("close.jpg",dilate)
+cv2.imwrite("assets/close.jpg",dilate)
 
 
 cv2.waitKey(0)
