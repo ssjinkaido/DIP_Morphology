c2b0253cfeb293630b6c24422e15c59a528bc084
ssjinkaido <53868217+ssjinkaido@users.noreply.github.com>
2020-11-29 08:25:40 +0700

    Update hitandmiss.py

--- hitandmiss.py
+++ hitandmiss.py
@@ -16,20 +16,20 @@ kernel_list = [
 
 
 def to_binary(image, thr):
-    """Convert a grayscale image to a binary image."""
+    
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.blur(image, (3, 3))
     return cv2.threshold(image, thr, 255, cv2.THRESH_BINARY_INV)[1]
 
 
 def hit_miss(image, kernel):
-    """Apply hit-or-miss transform on a binary image."""
+    
     image = cv2.morphologyEx(image, cv2.MORPH_HITMISS, kernel)
     return image
 
 
 if __name__ == "__main__":
-    img = cv2.imread('captcha.png')
+    img = cv2.imread('assets/captcha.png')
     cv2.imshow('img',img)
     img = to_binary(img, 80)
     plt.subplot(5, 3, 2)
